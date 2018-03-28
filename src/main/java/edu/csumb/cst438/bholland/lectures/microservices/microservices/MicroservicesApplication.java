59249cee2589d35ae18ac72510944afc664be3b1
Benjamin Holland <ben.holland.core@gmail.com>
2018-03-27 18:57:45 -0700

    made a thing!

--- src/main/java/edu/csumb/cst438/bholland/lectures/microservices/microservices/MicroservicesApplication.java
+++ src/main/java/edu/csumb/cst438/bholland/lectures/microservices/microservices/MicroservicesApplication.java
@@ -2,9 +2,21 @@ package edu.csumb.cst438.bholland.lectures.microservices.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.xml.ws.RequestWrapper;
+
+@Controller
 @SpringBootApplication
 public class MicroservicesApplication {
+
+	@RequestMapping("/")
+    @ResponseBody
+    String home(){
+	    return "Hello World! I am potato.";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroservicesApplication.class, args);
